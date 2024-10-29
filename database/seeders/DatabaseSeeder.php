a1981fd06291b7a0e15c553845e6d2f18b93bb01
asdw <asdwdev@gmail.com>
2024-10-29 13:51:25 +0700

    9. database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,58 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Sandhika Galih',
+            'email' => 'sandhikagalih@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+        User::create([
+            'name' => 'Doddy Ferdiansyah',
+            'email' => 'doddy@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate quis ex at expedita quae quisquam. Voluptates fugiat est quasi repellat dolore temporibus ut suscipit consequatur, soluta rerum, exercitationem, dolor laborum repellendus expedita sequi</p><p>excepturi nemo error quam culpa facere incidunt odio eum qui? Et quam amet placeat, vel dignissimos iure, velit assumenda laboriosam blanditiis veritatis harum? Sunt iste iure, consequatur illum obcaecati animi dolores cumque laborum autem ipsam sapiente quas velit sequi totam corrupti sit nam impedit voluptatem! Obcaecati, molestias error? Delectus eius</p><p>distinctio facere saepe, quis aliquam quia exercitationem nam voluptates repudiandae omnis in aliquid aperiam atque similique accusamus assumenda impedit corrupti pariatur ut repellendus optio voluptatem? Voluptates cupiditate iusto provident molestiae assumenda numquam quibusdam dolores nesciunt tenetur architecto deleniti distinctio fugit accusamus ipsa unde ad hic error recusandae laborum, vero veniam molestias cum? Neque excepturi quae sit aut. Voluptate nostrum beatae dolorem! Tempora?</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate quis ex at expedita quae quisquam. Voluptates fugiat est quasi repellat dolore temporibus ut suscipit consequatur, soluta rerum, exercitationem, dolor laborum repellendus expedita sequi</p><p>excepturi nemo error quam culpa facere incidunt odio eum qui? Et quam amet placeat, vel dignissimos iure, velit assumenda laboriosam blanditiis veritatis harum? Sunt iste iure, consequatur illum obcaecati animi dolores cumque laborum autem ipsam sapiente quas velit sequi totam corrupti sit nam impedit voluptatem! Obcaecati, molestias error? Delectus eius</p><p>distinctio facere saepe, quis aliquam quia exercitationem nam voluptates repudiandae omnis in aliquid aperiam atque similique accusamus assumenda impedit corrupti pariatur ut repellendus optio voluptatem? Voluptates cupiditate iusto provident molestiae assumenda numquam quibusdam dolores nesciunt tenetur architecto deleniti distinctio fugit accusamus ipsa unde ad hic error recusandae laborum, vero veniam molestias cum? Neque excepturi quae sit aut. Voluptate nostrum beatae dolorem! Tempora?</p>',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate quis ex at expedita quae quisquam. Voluptates fugiat est quasi repellat dolore temporibus ut suscipit consequatur, soluta rerum, exercitationem, dolor laborum repellendus expedita sequi</p><p>excepturi nemo error quam culpa facere incidunt odio eum qui? Et quam amet placeat, vel dignissimos iure, velit assumenda laboriosam blanditiis veritatis harum? Sunt iste iure, consequatur illum obcaecati animi dolores cumque laborum autem ipsam sapiente quas velit sequi totam corrupti sit nam impedit voluptatem! Obcaecati, molestias error? Delectus eius</p><p>distinctio facere saepe, quis aliquam quia exercitationem nam voluptates repudiandae omnis in aliquid aperiam atque similique accusamus assumenda impedit corrupti pariatur ut repellendus optio voluptatem? Voluptates cupiditate iusto provident molestiae assumenda numquam quibusdam dolores nesciunt tenetur architecto deleniti distinctio fugit accusamus ipsa unde ad hic error recusandae laborum, vero veniam molestias cum? Neque excepturi quae sit aut. Voluptate nostrum beatae dolorem! Tempora?</p>',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate',
+            'body' => '<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae nisi sint earum ullam cupiditate quis ex at expedita quae quisquam. Voluptates fugiat est quasi repellat dolore temporibus ut suscipit consequatur, soluta rerum, exercitationem, dolor laborum repellendus expedita sequi</p><p>excepturi nemo error quam culpa facere incidunt odio eum qui? Et quam amet placeat, vel dignissimos iure, velit assumenda laboriosam blanditiis veritatis harum? Sunt iste iure, consequatur illum obcaecati animi dolores cumque laborum autem ipsam sapiente quas velit sequi totam corrupti sit nam impedit voluptatem! Obcaecati, molestias error? Delectus eius</p><p>distinctio facere saepe, quis aliquam quia exercitationem nam voluptates repudiandae omnis in aliquid aperiam atque similique accusamus assumenda impedit corrupti pariatur ut repellendus optio voluptatem? Voluptates cupiditate iusto provident molestiae assumenda numquam quibusdam dolores nesciunt tenetur architecto deleniti distinctio fugit accusamus ipsa unde ad hic error recusandae laborum, vero veniam molestias cum? Neque excepturi quae sit aut. Voluptate nostrum beatae dolorem! Tempora?</p>',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
