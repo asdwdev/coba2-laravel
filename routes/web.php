1344e356ee9a5890df5ea7fb8af30cce7156f287
asdw <asdwdev@gmail.com>
2024-10-30 21:57:54 +0700

    14. view login & registration

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -61,3 +63,6 @@ Route::get('/categories', function () {
 //         'posts' => $author->posts->load('author', 'category'),
 //     ]);
 // });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
